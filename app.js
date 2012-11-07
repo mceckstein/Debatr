070bb5628afd19e4cb3a9580ba16cca9625281bf
matte <matte@matte.com>
2012-11-06 20:27:16 -0800

    Clean up unneeded files

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -10,8 +9,7 @@ var express = require('express'),
 var app = module.exports = express();
 
 // Configuration
-
-app.configure(function(){
+app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
@@ -20,21 +18,22 @@ app.configure(function(){
   app.use(app.router);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('development', function() {
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
 });
 
-app.configure('production', function(){
+app.configure('production', function() {
   app.use(express.errorHandler());
 });
 
 // Routes
-
 app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
 
 // JSON API
-
 app.get('/api/posts', api.posts);
 
 app.get('/api/post/:id', api.post);
@@ -46,7 +45,6 @@ app.delete('/api/post/:id', api.deletePost);
 app.get('*', routes.index);
 
 // Start server
-
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
--- routes/api.js
+++ routes/api.js
@@ -3,17 +3,15 @@
  */
 
 var database = {
-	"posts": [
-		{
-			"title": "Foobar",
-			"text": "BAZ BAT"
-		}
-	]
+	"posts": [{
+		"title": "Foobar",
+		"text": "BAZ BAT"
+	}]
 };
 
 exports.posts = function(req, res) {
 	var posts = [];
-	database.posts.forEach(function (post, i) {
+	database.posts.forEach(function(post, i) {
 		posts.push({
 			id: i,
 			title: post.title,
@@ -27,7 +25,7 @@ exports.posts = function(req, res) {
 
 exports.post = function(req, res) {
 	var id = req.params.id;
-	if (id >= 0 && id < database.posts.length) {
+	if(id >= 0 && id < database.posts.length) {
 		res.json({
 			post: database.posts[id]
 		});
@@ -37,31 +35,31 @@ exports.post = function(req, res) {
 };
 
 // POST
-exports.addPost = function (req, res) {
-  database.posts.push(req.body);
-  res.json(req.body);
+exports.addPost = function(req, res) {
+	database.posts.push(req.body);
+	res.json(req.body);
 };
- 
+
 // PUT
-exports.editPost = function (req, res) {
-  var id = req.params.id;
-  
-  if (id >= 0 && id < database.posts.length) {
-    database.posts[id] = req.body;
-    res.json(true);
-  } else {
-    res.json(false);
-  }
+exports.editPost = function(req, res) {
+	var id = req.params.id;
+
+	if(id >= 0 && id < database.posts.length) {
+		database.posts[id] = req.body;
+		res.json(true);
+	} else {
+		res.json(false);
+	}
 };
- 
+
 // DELETE
-exports.deletePost = function (req, res) {
-  var id = req.params.id;
-  
-  if (id >= 0 && id < database.posts.length) {
-    database.posts.splice(id, 1);
-    res.json(true);
-  } else {
-    res.json(false);
-  }
+exports.deletePost = function(req, res) {
+	var id = req.params.id;
+
+	if(id >= 0 && id < database.posts.length) {
+		database.posts.splice(id, 1);
+		res.json(true);
+	} else {
+		res.json(false);
+	}
 };
